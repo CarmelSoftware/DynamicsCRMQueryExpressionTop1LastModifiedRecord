45de2ae218755412bd6a49d5a3b4f14de60fbdef
Carmel Shvartzman <carmel.aspnet@gmail.com>
2016-02-03 21:51:52 +0200

    Update QueryExpression.cs

--- QueryExpression.cs
+++ QueryExpression.cs
@@ -1,1 +1,14 @@
 
+Entity oLetter = null;
+           
+            try
+            {
+
+                ColumnSet columns = new ColumnSet(
+                    new string[4] 
+                    { 
+                        "activityid", 
+                        "modifiedon", 
+                        "createdon", 
+                        "new_myattr" 
+                    });
